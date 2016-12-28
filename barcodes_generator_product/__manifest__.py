78e8eb5a9ccbe78883c5dee9b1fe7d1d0956ac1d
Dave Lasley <dave@laslabs.com>
2016-12-28 13:52:41 -0800

    barcodes_product_generator: Upgrade to v10 * Rename manifest * Change openerp references to odoo * Bump version

--- barcodes_generator_product/__manifest__.py
+++ barcodes_generator_product/__manifest__.py
@@ -7,7 +7,7 @@
 {
     'name': 'Generate Barcodes for Products',
     'summary': 'Generate Barcodes for Products (Templates and Variants)',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Tools',
     'author':
         'GRAP,'
